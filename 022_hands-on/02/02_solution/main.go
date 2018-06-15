bdb95bddc6a8dc1f72ec25c59420234f350e07ed
just331 <justinr331@gmail.com>
2018-06-14 19:37:16 -0500

    GO lang web dev

--- 022_hands-on/02/02_solution/main.go
+++ 022_hands-on/02/02_solution/main.go
@@ -1,26 +1,27 @@
 package main
 
 import (
-	"net"
-	"log"
 	"io"
+	"log"
+	"net"
 )
 
 func main() {
-	l, err := net.Listen(tcp, ":8080")
-	if err != nil{
+	l, err := net.Listen("tcp", ":8080")
+	if err != nil {
 		log.Fatalln(err)
 	}
 	defer l.Close()
 
 	for {
 		conn, err := l.Accept()
-		if err != nil{
+		if err != nil {
 			log.Println(err)
 			continue
 		}
 
-		io.WriteString(conn, "You are connected!")
+		// write to connection
+		io.WriteString(conn, "I see you connected.")
 
 		conn.Close()
 	}

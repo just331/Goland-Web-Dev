69dcfa476e48564865b74af77c4845b38e913d94
just331 <justinr331@gmail.com>
2018-06-14 19:19:59 -0500

    GO lang web dev

---
+++ 022_hands-on/02/01_hands-on/main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"net"
+	"log"
+	"io"
+)
+
+func main() {
+	l, err := net.Listen(tcp, ":8080")
+	if err != nil{
+		log.Fatalln(err)
+	}
+	defer l.Close()
+
+	for {
+		conn, err := l.Accept()
+		if err != nil{
+			log.Println(err)
+			continue
+		}
+
+		io.WriteString(conn, "You are connected!")
+
+		conn.Close()
+	}
+}
